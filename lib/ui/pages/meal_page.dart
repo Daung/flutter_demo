45c7f96c53ff0c21401d8002e8953b2d55fc27fa
wxc <wxc@yiban.cn>
2021-04-30 16:21:00 +0800

    consumer 专程selector 进行数据共享

--- lib/ui/pages/meal_page.dart
+++ lib/ui/pages/meal_page.dart
@@ -1,6 +1,8 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:study_flutter_003/core/model/category.dart';
+import 'package:study_flutter_003/core/model/meal.dart';
 import 'package:study_flutter_003/core/viewmodel/meal_view_model.dart';
 
 class MealDetailPage extends StatelessWidget {
@@ -15,18 +17,22 @@ class MealDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Consumer<WZMealViewModel>(builder: (context, mealVM, child) {
-        final meals = mealVM.meals
-            .where((element) => element.categories.contains(category.id))
-            .toList();
-        return ListView.builder(
-            itemCount: meals.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(meals[index].title),
-              );
-            });
-      }),
+      body: Selector<WZMealViewModel, List<WZMealModel>>(
+        //selector 就是过滤的函数
+          selector: (context, mealVM) => mealVM.meals
+              .where((element) => element.categories.contains(category.id))
+              .toList(),
+          //ListEquality 判断两个list是否相等
+          shouldRebuild: (prev, next) => ListEquality().equals(prev, next),
+          builder: (context, meals, child) {
+            return ListView.builder(
+                itemCount: meals.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(meals[index].title),
+                  );
+                });
+          }),
     );
   }
 }
